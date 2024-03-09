12c0cd49fb29ec533b82dac5e27d0ff3a2387394
AP <Alexanderp2026@student.cis.edu.hk>
2024-03-09 17:27:48 +0800

    hi

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,89 @@
 public class Student {
+    private int id;
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    private int yearLevel;
+
+    private static int studentCount;
+
+    public Student(String firstName, String lastName, int age, int yearLevel)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+    }
+
+    public int getID()
+    {
+        return id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public int getYearLevel()
+    {
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel)
+    {
+        this.yearLevel = yearLevel;
+    }
+
+    public static int getStudentCount()
+    {
+        return studentCount;
+    }
+
+    public boolean equals(Student student)
+    {
+        if (firstName.equals(student.firstName) && lastName.equals(student.lastName) && age == student.age && yearLevel == student.yearLevel)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString()
+    {
+        return "Student{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "', age=" + age + ", yearLevel=" + yearLevel + "}";
+    }
+
+
+
 
 
 }
